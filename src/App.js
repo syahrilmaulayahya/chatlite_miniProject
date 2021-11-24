9b094c837d2dccaf44ae08319191f6101dd00e02
syahrilmaulayahya <syahrilmaulayahya@gmail.com>
2021-11-24 15:23:50 +0700

    test login

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <button
+          className="App-link"
+          onClick={() => {loginWithRedirect()}}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+ReactDOM.render(
+  
+  <Auth0Provider
+    domain="dev-m-ead5z8.us.auth0.com"
+    clientId="CKR9ZNPo8kU5ExllYq7TgbwSHBfOz1rs"
+    redirectUri="http://localhost:3000"
+    scope="read:current_user update:current_user_metadata"
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
